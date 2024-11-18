609205c8156f86f59a94e7e64235e2a767d34b78
zhangfeng <zhangfeng1470@163.com>
2024-11-18 18:14:42 +0800

    add

--- driver/wx_can.c
+++ driver/wx_can.c
@@ -4,9 +4,104 @@
 #include "xparameters.h"
 #include "xil_printf.h"
 
+/* 设置CAN中断 */
+UINT32 WX_CAN_SetupCanInterrupt(XCanPs *canInstPtr, WxCanIntrCfgInfo *intrCfg)
+{
+	/* 中断控制器 */
+	INTC *intcInst = WX_GetIntrCtrlInst();
+	if (intcInst == NULL) {
+		return WX_UART_NS550_INTR_CTRL_UNREADY;
+
+	}
+
+	/*
+	 * Connect the device driver handler that will be called when an
+	 * interrupt for the device occurs, the handler defined above performs
+	 * the specific interrupt processing for the device.
+	 */
+	int status = XScuGic_Connect(intcInst, intrCfg->intrId,
+				 (Xil_ExceptionHandler)XCanPs_IntrHandler, canInstPtr);
+	if (status != XST_SUCCESS) {
+		wx_log(WX_CRITICAL, "Error Exit: XScuGic_Connect RS422(%u) fail(%u)", status);
+		return WX_SCUGIC_CONNECT_RS422_FAIL;
+	}
+
+	/*
+	 * Enable the interrupt for the Timer device.
+	 */
+	XScuGic_Enable(intcInst, intrCfg->intrId);
+	
+	/*
+	 * Enable all interrupts in CAN device.
+	 */
+	XCanPs_IntrEnable(canInstPtr, XCANPS_IXR_ALL);
+	/*
+	 * Set interrupt handlers.
+	 */
+	XCanPs_SetHandler(canInstPtr, XCANPS_HANDLER_SEND, (void *)SendHandler, (void *)CanInstPtr);
+	XCanPs_SetHandler(canInstPtr, XCANPS_HANDLER_RECV, (void *)RecvHandler, (void *)CanInstPtr);
+	XCanPs_SetHandler(canInstPtr, XCANPS_HANDLER_ERROR, (void *)ErrorHandler, (void *)CanInstPtr);
+	XCanPs_SetHandler(canInstPtr, XCANPS_HANDLER_EVENT, (void *)EventHandler, (void *)CanInstPtr);
+	return WX_SUCCESS;
+}
+
+/* CAN发送数据帧, 该函数发送失败就会返回对应的原因值 */
+UINT32 WX_CAN_SendFrame(XCanPs *canInstPtr, WxCanFrame *frame)
+{
+	if (canInstPtr == NULL) {
+		return WX_CAN_CONFIG_INVALID_CAN_INST_PRT;
+	}
+	/*
+	* Buffers to hold frames to send and receive. These are declared as global so
+	* that they are not on the stack.
+	* These buffers need to be 32-bit aligned
+	*/
+	UINT32 txFrame[WX_CAN_FRAME_U32_NUM] = {0};
+	/*
+	 * Create correct values for Identifier and Data Length Code Register.
+	 */
+	txFrame[0] = (u32)XCanPs_CreateIdValue(frame->standardMessID, frame->subRemoteTransReq,
+		frame->idExtension, frame->extendedId, frame->remoteTransReq);
+	txFrame[1] = (u32)XCanPs_CreateDlcValue(frame->dataLengCode);
+
+	/*
+	 * Now fill in the data field with known values so we can verify them
+	 * on receive.
+	 */
+	UINT8 *dataPtr = (UINT8 *)(&txFrame[2]);
+	for (UINT32 i = 0; i < frame->dataLengCode; i++) {
+		*dataPtr++ = (u8)frame->data[i];
+	}
+
+	
+	/*
+	 * if TX FIFO is full send will fail.
+	 */
+	if (XCanPs_IsTxFifoFull(canInstPtr) == TRUE) {
+		return WX_CAN_CONFIG_TX_BUFF_FULL;
+	}
+
+	/*
+	 * Now send the frame.
+	 *
+	 * Another way to send a frame is keep calling XCanPs_Send() until it
+	 * returns XST_SUCCESS. No check on if TX FIFO is full is needed anymore
+	 * in that case.
+	 */
+	UINT32 status = XCanPs_Send(canInstPtr, txFrame);
+	if (status != XST_SUCCESS) {
+		return WX_CAN_CONFIG_TX_FAIL_FULL;
+	}
+
+	return WX_SUCCESS;
+}
+
 /* CAN设备初始化 */
 UINT32 WX_CAN_InitialDevice(XCanPs *canInstPtr, WxCanDeviceCfgInfo *cfg)
 {
+	if (canInstPtr == NULL) {
+		return WX_CAN_CONFIG_INVALID_CAN_INST_PRT;
+	}
 	/*
 	 * Initialize the Can device.
 	 */
--- task/can_slave/wx_can_slave_task.h
+++ task/can_slave/wx_can_slave_task.h
@@ -1,12 +1,19 @@
 #ifndef WX_CAN_SLAVE_TASK_H
 #define WX_CAN_SLAVE_TASK_H
+#include "wx_can.h"
+typedef enum {
+    WX_CAN_TYPE_A,
+    WX_CAN_TYPE_B,
+    WX_CAN_TYPE_BUTT,
+} WxCanTypeDef;
 
 typedef struct {
-
-};
-
-typedef struct {
-    UINT32 
+    WxCanDeviceCfgInfo deviceCfgInfo; /* CAN设备配置信息 */ 
+    WxCanIntrCfgInfo intrCfgInfo; /* 中断配置信息 */
 } WxCanSlaveCfgInfo;
 
+/* CAN从机任务信息 */
+typedef struct {
+    ;
+} WxCanSlaveTask;
 #endif
