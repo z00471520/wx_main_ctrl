226808adcbbfdd0eb59820c9634520f233f4812b
zhangfeng <zhangfeng1470@163.com>
2024-11-28 14:23:51 +0800

    add

--- driver/wx_rs422_driver/wx_rs422_driver_master.h
+++ driver/wx_rs422_driver/wx_rs422_driver_master.h
@@ -11,7 +11,6 @@ typedef struct {
     UINT8 status; /* 状态 */
     WxModbusAdu txAdu; /* the adu to be send by rs422 */
     WxModbusAdu rxAdu;  /* the adu recieve from rs422 */
-    WxRs422IMasterMsg rs422Msg; /* A buffer used to store the tx/rx msg from the other module */
     QueueHandle_t msgQueHandle; /* 用于缓存外部模块发送给RS422的待发送的消息队列 */
     XUartNs550 rs422Inst; /* RS422实例 */
     UINT64 slaveExcpCnt[WX_RS422_MASTER_MB_OPR_BUTT][WX_RS422_MASTER_SLAVE_ADDR_BUTT][WX_MODBUS_MAX_EXCP_CODE_NUM]; /* 用于记录不同操作不同从机返回的异常码 */
